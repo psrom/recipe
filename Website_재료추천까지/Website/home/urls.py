cdf6a211350a50066a410818c9ba5e88b77a9ddf
JIEUNNN27 <icecream7593@gmail.com>
2022-05-19 15:45:18 +0900

    웹 재료추천까지

---
+++ Website_재료추천까지/Website/home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns=[
+    path('', views.index, name='index'),
+    path('api/ingredient/', views.ingredientsjson, name='ingredient_json'),
+    path('ing_rec/', views.rec, name = 'ingre'),
+
+]
---
+++ Website_재료추천까지/Website/model_data/r_v.p
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3e7d031ac485a435bd97eeddabe49fc136d0193af276d91a55972be46b92445d
+size 606992142
