3a33edca7096268842f949206aac3006fdc4e74b
psrom <psrom721@gmail.com>
2022-05-24 13:36:48 +0900

    home/views.py 홈페이지 랜덤 레시피 추가

--- Website/home/views.py
+++ Website/home/views.py
@@ -9,12 +9,19 @@ from recipe_model import *
 import requests
 from bs4 import BeautifulSoup
 
+import random
+
 # Create your views here.
 def index(request):
+    RECIPE_NUM=8
     #print(request.user)
     #Ingredient.objects.get().hate_users()
     #print(request.user.hate_ingredients.all())
-    return render(request, 'index.html')
+    rand_val = random.randint(0, 10000)
+
+
+    default_recipe = Home.objects.filter().all()[rand_val:rand_val+RECIPE_NUM] # random 8개 
+    return render(request, 'index.html', {"context": default_recipe})
 
     
 def ingredientsjson(request):
@@ -26,16 +33,48 @@ def ingredientsjson(request):
 
     return HttpResponse(serialize('json', queryset=ingredient))
 
+# def personToDictionary(person):
+#     if person == None:
+#         return None
+
+#     dictionary = {}
+#     dictionary["username"] = person.username
+#     dictionary["firstName"] = person.firstName
+#     dictionary["middleName"] = person.middleName
+#     dictionary["lastName"] = person.lastName
+#     dictionary["age"] = person.age
+
+#     return dictionary
+
+# person = Person.objects.get(id = 25)
+# personDictionary = personToDictionary(person)
+
+# from django.core import serializers
+
+# person = serializers.serialize("json", Person.objects.get(id = 25))
+# people = serializers.serialize("json", Person.objects.all())
+
+# from django.core import serializers
+
+# person = serializers.serialize("json", Person.objects.get(id = 25), fields = ("firstName", "middleName", "lastName"))
+# people = serializers.serialize("json", Person.objects.all(), fields = ("firstName", "middleName", "lastName"))
 
 
 import json
 
 def rec(request):
-    
+    # k = request.GET.get('input-custom-dropdown')
+    # i = [eval(k)[i]['value'].split(',') for i in range(len(eval(k)))]
+    # i = sum(i, [])
+    # print(eval(k))
+    # print(len(eval(k)))
+    # print(i)
+
+    # jar, result = Ing_rec(i).rec()
     
     if not request.GET.get('ingre'):
         ingredient = Ingredient.objects.all()
-
+        # raise HttpResponseNotFound("Required ingredient")
     else:
         data = request.GET.get('ingre')
         data = data.split(',') # list
@@ -51,6 +90,7 @@ def rec(request):
     return HttpResponse(json.dumps({'result':result, 'jar':round(jar,3)}), content_type="application/json")
     
 
+    # return render(request, 'index.html', {'result':result, 'jar':round(jar,3), 'list':i})
 
 
 
@@ -97,13 +137,10 @@ def recipe_rec(request):
 
     context = []
     for i in range(len(image_url)):
-
         dic = {}
-        dic['id'] = recc[i].id
         dic['name'] =recc[i].name
         dic['ingredients'] = recc[i].ingredients
         dic['url'] = recc[i].url
-        dic['cluster'] = recc[i].cluster
         dic['img_url'] = image_url[i]
         context.append(dic) 
 
